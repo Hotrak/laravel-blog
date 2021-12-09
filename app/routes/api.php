5cb6bb0031a5e73ab92410622f896813c1be1b54
Тумановский Владислав Сергеевич <v.tumanovski@kali.by>
2021-12-09 17:40:23 +0300

    test commit

--- app/routes/api.php
+++ app/routes/api.php
@@ -20,6 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/posts', [PostController::class, 'index']);
-
 Route::resource('cards', CardController::class);
