ef16d1d492a3f694bbf9b810b23801c3770a66c7
Тумановский Владислав Сергеевич <v.tumanovski@kali.by>
2021-12-09 16:42:33 +0300

    add base models and migrations

---
+++ app/database/migrations/2021_12_09_130821_create_cards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->float('balance')->default(0);
+            $table->unsignedBigInteger('currency_type_id');
+            $table->timestamps();
+
+            $table->foreign('currency_type_id')->references('id')->on('currency_types');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
